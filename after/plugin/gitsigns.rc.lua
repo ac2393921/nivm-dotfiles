1fc2061336b454ff4d33b086e80008621f3812e7
ac2393921 <ac2393921@gmail.com>
2022-11-25 00:58:58 +0900

    updateL keysigns

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -1,4 +1,13 @@
 local status, gitsigns = pcall(require, "gitsigns")
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+  signs = {
+    -- TODO add hl to colorscheme
+    add          = { hl = 'GitSignsAdd', text = ' +', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete       = { hl = 'GitSignsDelete', text = ' -', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete    = { hl = 'GitSignsDelete', text = ' -', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+  },
+}
